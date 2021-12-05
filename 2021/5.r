815a1c98a10440854ee5bb9620b726c7fbe77de1
iarwain <iarwain@orx-project.org>
2021-12-05 00:53:04 -0500

    05-12-2021

---
+++ 2021/5.r
@@ -0,0 +1,43 @@
+REBOL [
+  Title: {Day 5}
+  Date: 05-12-2021
+]
+
+data: map-each entry load replace/all replace/all read %data/5.txt {,} {x} {->} {} [entry + 1x1]
+
+; --- Part 1 ---
+count: funct [/all] [
+  bound: 0x0 foreach pos data [bound: max pos bound]
+  grid: array/initial reduce [bound/x bound/y] 0
+  foreach [begin end] data [
+    delta: end - begin
+    if any [
+      all
+      delta/x = 0
+      delta/y = 0
+    ] [
+      pos: begin - inc: as-pair sign? delta/x sign? delta/y
+      until [
+        pos: pos + inc
+        grid/(pos/x)/(pos/y): grid/(pos/x)/(pos/y) + 1
+        pos = end
+      ]
+    ]
+  ]
+  result: 0
+  foreach line grid [
+    foreach entry line [
+      if entry > 1 [
+        result: result + 1
+      ]
+    ]
+  ]
+  result
+]
+
+r1: count
+print [{Part 1:} r1]
+
+; --- Part 2 ---
+r2: count/all
+print [{Part 2:} r2]
